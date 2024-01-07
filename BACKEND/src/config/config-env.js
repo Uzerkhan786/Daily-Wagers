e668b3d529b07f10095a1b7d5749beed4bf10839
Uzerkhan786 <uzer.khan2020@vitbhopal.ac.in>
2024-01-07 11:46:47 +0530

    adding .env

--- BACKEND/src/config/config-env.js
+++ BACKEND/src/config/config-env.js
@@ -1,4 +1,6 @@
 require('dotenv').config();
 module.exports={
-    PORT:process.env.PORT
+    PORT:process.env.PORT || 3000,
+    MONGO_DB_URI:process.env.MONGO_DB_URI ,
+    BASE_URL:process.env.BASE_URL
 }
--- BACKEND/src/config/db.js
+++ BACKEND/src/config/db.js
@@ -1,9 +1,19 @@
 const mongoose=require('mongoose');
+const { MONGO_DB_URI } = require('./config-env');
 
 const mongoDbConnection=async()=>{
+    // try {
+    //     await mongoose.connect('mongodb://localhost:27017/dailyWagers');
+    //     console.log('Database is connected');
+    // } catch (error) {
+    //     console.log('Something went wrong',error);
+    // }
     try {
-        await mongoose.connect('mongodb://localhost:27017/dailyWagers');
-        console.log('Database is connected');
+        await mongoose.connect(MONGO_DB_URI,{
+            useUnifiedTopology:true,
+            useNewUrlParser:true
+        });
+         console.log('Database is connected');
     } catch (error) {
         console.log('Something went wrong',error);
     }
