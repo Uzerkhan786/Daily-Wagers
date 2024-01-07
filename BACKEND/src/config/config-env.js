4fdc75bb2cf9b653575ad5a3d9737e2731b50f07
Uzerkhan786 <uzer.khan2020@vitbhopal.ac.in>
2024-01-07 12:37:15 +0530

    changes

--- BACKEND/src/config/config-env.js
+++ BACKEND/src/config/config-env.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 module.exports={
-    PORT:process.env.PORT || 3000,
+    PORT:process.env.PORT || 3001,
     MONGO_DB_URI:process.env.MONGO_DB_URI ,
     BASE_URL:process.env.BASE_URL
 }
